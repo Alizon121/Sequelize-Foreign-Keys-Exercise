d26011058a25e0cd9cc27d497d23d47da7a0b4a5
Andrew <andrew.lizon12@gmail.com>
2024-09-22 15:15:04 -0700

    Removed intrumentId and musiciansId from the musican and instrument models

--- server/db/models/instrument.js
+++ server/db/models/instrument.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Instrument extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Instrument.belongsToMany(
         models.Musician,
@@ -25,10 +20,6 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
-    },
-    musicianId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
     }
   }, {
     sequelize,
--- server/db/models/musician.js
+++ server/db/models/musician.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Musician extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Musician.belongsTo(
         models.Band,
@@ -30,10 +25,6 @@ module.exports = (sequelize, DataTypes) => {
     bandId: {
      type: DataTypes.INTEGER,
      allowNull: false
-    },
-    instrumentId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
     }
   }, {
     sequelize,
