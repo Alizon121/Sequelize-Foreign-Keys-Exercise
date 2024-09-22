327fac4e78cf53b93985eb2816543476957a6d2d
Andrew <andrew.lizon12@gmail.com>
2024-09-22 13:04:58 -0700

    Added musicianInstrument migration/model

---
+++ server/db/models/musicianinstruments.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class MusicianInstruments extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  MusicianInstruments.init({
+    musicianId: DataTypes.INTEGER,
+    instrumentId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'MusicianInstruments',
+  });
+  return MusicianInstruments;
+};
